76dd02429c71871cef46016e54e58fcfa1c5624a
FraBila <francescobilardo1@gmail.com>
2025-06-22 16:10:33 +0200

    Ottimizzazione e creazione struttura dei punti vendita
    Punti vendita provvisori

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,3 @@
-
 CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START 1 INCREMENT 1;
 
 
@@ -8,4 +7,6 @@ CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START 1 INCREMENT 1;
 -- Inserimento delle credenziali collegate all'utente appena creato
 --insert into credentials (id_cliente, id_dipendente, id, password_encode, ruolo, username) values (currval('hibernate_sequence'), null, nextval('hibernate_sequence'), '$2a$10$gKGUATrqKdpK3pX8lTRcCeOHk4dSL1I5KPc.csNEjYibvxT1Yw5KK', 'CLIENT', 'jacoclem');
 
-insert into punto_vendita (id, citta, indirizzo) values  (nextval('hibernate_sequence'), 'Roma', 'Via rossi');
+insert into punto_vendita (id, citta, indirizzo) values  (nextval('hibernate_sequence'), 'Roma', 'Via Rossi 55');
+insert into punto_vendita (id, citta, indirizzo) values  (nextval('hibernate_sequence'), 'Milano', 'Via Bianchi 44');
+insert into punto_vendita (id, citta, indirizzo) values  (nextval('hibernate_sequence'), 'Napoli', 'Via Verdi 33');
